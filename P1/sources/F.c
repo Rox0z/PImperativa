ab2becbcfd88b385ee594c92838de8ec3d47b529
Leonardo Marinho <leonxlok1@hotmail.com>
2022-09-30 21:48:44 +0100

    TODO: optmizar e simplificar

--- P1/sources/F.c
+++ P1/sources/F.c
@@ -1,31 +1,34 @@
-#include <stdio.h>
+//! Código interditado para reformas!!!! (simplificar e optmizar)
 
-const char *author = "Leonardo Albudane";
+// #include <stdio.h>
 
-int succ(int x);
-int pred(int x);
-int is_zero(int x);
-int is_pos(int x);
+// const char *author = "Leonardo Albudane";
 
-int dpred(int x) { // Retorna X - 2
-    return pred(pred(x));
-}
+// int succ(int x);
+// int pred(int x);
+// int is_zero(int x);
+// int is_pos(int x);
 
-int dsucc(int x) { // Retorna X + 2
-    return succ(succ(x));
-}
+// int dpred(int x) { // Retorna X - 2
+//     return pred(pred(x));
+// }
 
-int half(int x, int y) {
-    return is_zero(y)               // Verifica se é zero
-    ? x                             // Se sim retorna X
-    : is_pos(x)                     // Se não verifica se é positivo
-        ? is_pos(y) ? half(pred(x), dpred(y)) : x       // Se sim refaz o comando com X-1 e Y-2
-        : is_pos(y) ? x : half(succ(x), dsucc(y));      // Se não refaz o comando com X+1 e Y-2
-}
+// int dsucc(int x) { // Retorna X + 2
+//     return succ(succ(x));
+// }
 
-int main(void) {
-    int x;
-    scanf("%d", &x);
-    printf("%d\n",half(x, x));
-    return 0;
-}
+// int half(int x, int y) {
+//     return is_zero(y)               // Verifica se é zero
+//     ? x                             // Se sim retorna X
+//     : is_pos(x)                     // Se não verifica se é positivo
+//         ? is_pos(y) ? half(pred(x), dpred(y)) : x       // Se sim refaz o comando com X-1 e Y-2
+//         : is_pos(y) ? x : half(succ(x), dsucc(y));      // Se não refaz o comando com X+1 e Y-2
+// }
+
+// int main(void) {
+//     int x;
+//     scanf("%d", &x);
+//     printf("%d\n",half(x, x));
+//     return 0;
+// }
+
--- P1/sources/H.c
+++ P1/sources/H.c
@@ -1,33 +1,35 @@
-#include <stdio.h>
+//! Código interditado para reformas!!!! (simplificar e optmizar)
 
-const char *author = "Leonardo Albudane";
+// #include <stdio.h>
 
-int succ(int x);
-int pred(int x);
-int is_zero(int x);
-int is_pos(int x);
+// const char *author = "Leonardo Albudane";
 
-int sum(int x, int y) { // Doc no exercicio B
-    return is_zero(y) ? x : sum(succ(x), pred(y));
-}
+// int succ(int x);
+// int pred(int x);
+// int is_zero(int x);
+// int is_pos(int x);
 
-int mult(int x, int y) { 
-    return is_zero(x) || is_zero(y)     // Verifica se X ou Y são zero
-    ? 0                                 // Se sim retorna 0 (0 * n == 0)
-    : sum(x, mult(x, pred(y)));         // Se não soma com X e multiplicação de X com Y-1
-}
+// int sum(int x, int y) { // Doc no exercicio B
+//     return is_zero(y) ? x : sum(succ(x), pred(y));
+// }
 
-int power(int x, int y) {
-    return is_zero(y)                   // Verifica se Y == 0
-    ? 1                                 // Se sim retorna 1
-    : is_zero(pred(y))                  // Se não verifica se Y-1 == 0 (Y == 1)
-        ? x                                 // Se sim retorna X (X ** 1 == X)
-        : mult(power(x, pred(y)), x);       // Se não retorna multiplicação da potência de X e Y-1 com X
-}
+// int mult(int x, int y) { 
+//     return is_zero(x) || is_zero(y)     // Verifica se X ou Y são zero
+//     ? 0                                 // Se sim retorna 0 (0 * n == 0)
+//     : sum(x, mult(x, pred(y)));         // Se não soma com X e multiplicação de X com Y-1
+// }
 
-int main(void) {
-    int x;
-    scanf("%d", &x);
-    printf("%d\n", power(succ(1), x));
-    return 0;
-}
+// int power(int x, int y) {
+//     return is_zero(y)                   // Verifica se Y == 0
+//     ? 1                                 // Se sim retorna 1
+//     : is_zero(pred(y))                  // Se não verifica se Y-1 == 0 (Y == 1)
+//         ? x                                 // Se sim retorna X (X ** 1 == X)
+//         : mult(power(x, pred(y)), x);       // Se não retorna multiplicação da potência de X e Y-1 com X
+// }
+
+// int main(void) {
+//     int x;
+//     scanf("%d", &x);
+//     printf("%d\n", power(succ(1), x));
+//     return 0;
+// }
