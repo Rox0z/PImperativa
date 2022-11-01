a46d8307a21b80e230685bf16b6a3b6ef928505c
Leonardo Marinho <leonxlok1@hotmail.com>
2022-11-01 17:24:10 +0000

    P5

---
+++ P5/sources/C.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+void minmax(double *ar, int size)
+{
+    double max = ar[0], min = ar[0];
+    for (int i = 0; i < size; i++)
+    {
+        max = max < ar[i] ? ar[i] : max;
+        min = min > ar[i] ? ar[i] : min;
+    }    
+    printf("%f\n%f\n%f\n", max, min, max-min);
+}
+
+void test(void)
+{
+    double ar[20] = {};
+    int i = 0;
+    while (scanf("%lf", &ar[i]) != EOF)
+        i++;
+
+    minmax(ar, i);
+}
+
+int main(void)
+{
+    test();
+    return 0;
+}
