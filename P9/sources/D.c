95fe2a41f65b42d8a16a6f45f616f450c549ac97
Leonardo Marinho <leonxlok1@hotmail.com>
2022-11-16 17:56:46 +0000

    assert

--- P9/sources/D.c
+++ P9/sources/D.c
@@ -15,14 +15,22 @@ void return_print(const int *ar, int j)
     printf("\n");
 }
 
-int ints_argsmax(const int *a, int n)
+int ints_argsmax(const int *a, int n, int index)
 {
     int ar[100] = {}, j = 0, max = max_value(a, n - 1, 0);
     for (int i = 0; i < n; i++)
         a[i] == max ? (ar[j] = i, j++) : 0;
 
-    return_print(ar, j);
-    return ar[0];
+    return ar[index];
+}
+
+int ints_argsmax_times(const int *a, int n)
+{
+    int j = 0, max = max_value(a, n - 1, 0);
+    for (int i = 0; i < n; i++)
+        a[i] == max ? (j++) : 0;
+
+    return j;
 }
 
 void test(void)
@@ -31,22 +39,38 @@ void test(void)
 
     while (scanf("%d", &x[i]) != EOF)
         i++;
-    ints_argsmax(x, i);
+
+    int times = ints_argsmax_times(x, i);
+
+    printf("%d", times);
+
+    for (int j = 0; j < times; j++)
+        printf(" %d", ints_argsmax(x, i, j));
+    printf("\n");
 }
 
 void unit_test_ints_argsmax(void)
 {
-    // int a0[8] = {1, 2, 3, 4, 5, 6, 7, 8};
-    // int a1[8] = {8, 7, 6, 5, 4, 3, 2, 1};
-    // int a2[8] = {0, 0, 0, 0, 0, 1, 0, 0};
-    // int a3[8] = {1, 1, 1, 1, 1, 0, 1, 1};
-    // int a4[10] = {3, 4, 5, 9, 7, 2, 9, 3, 1, 6};
+    int a0[8] = {1, 2, 3, 4, 5, 6, 7, 8};
+    int a1[8] = {8, 7, 6, 5, 4, 3, 2, 1};
+    int a2[8] = {0, 0, 0, 0, 0, 1, 0, 0};
+    int a3[8] = {1, 1, 1, 1, 1, 0, 1, 1};
+    int a4[10] = {3, 4, 5, 9, 7, 2, 9, 3, 1, 6};
 
-    // assert(ints_argsmax(a0, 8) == 7);
-    // assert(ints_argsmax(a1, 8) == 0);
-    // assert(ints_argsmax(a2, 8) == 5);
-    // assert(ints_argsmax(a3, 8) == 0);
-    // assert(ints_argsmax(a4, 10) == 3);
+    assert(ints_argsmax_times(a0, 8) == 1);
+    assert(ints_argsmax(a0, 8, 0) == 7);
+
+    assert(ints_argsmax_times(a1, 8) == 1);
+    assert(ints_argsmax(a1, 8, 0) == 0);
+
+    assert(ints_argsmax_times(a2, 8) == 1);
+    assert(ints_argsmax(a2, 8, 0) == 5);
+
+    assert(ints_argsmax_times(a3, 8) == 7);
+    assert(ints_argsmax(a3, 8, 0) == 0);
+
+    assert(ints_argsmax_times(a4, 10) == 2);
+    assert(ints_argsmax(a4, 10, 0) == 3);
 }
 
 int main(void)
--- P9/sources/E.c
+++ P9/sources/E.c
@@ -1,21 +1,22 @@
 #include <assert.h>
 #include <stdio.h>
 
-void return_print(const int *ar, int j, int total)
-{
-    printf("%d", total);
-    for (int i = 0; i < j; i++)
-        printf(" %d", ar[i]);
-    printf("\n");
-}
-
-void sum_arrays(const int *a, int n)
+int sum_arrays_total(const int *a, int n)
 {
     int ar[100] = {}, arr[100] = {}, total = 0;
     for (int i = 0; i < n; i++)
         (ar[i] = a[i] * a[i + 1 == n ? n - 1 : i + 1], arr[i] = a[i] + ar[i], total += arr[i]);
 
-    return_print(arr, n, total);
+    return total;
+}
+
+int sum_arrays(const int *a, int n, int index)
+{
+    int ar[100] = {}, arr[100] = {};
+    for (int i = 0; i < n; i++)
+        (ar[i] = a[i] * a[i + 1 == n ? n - 1 : i + 1], arr[i] = a[i] + ar[i]);
+
+    return arr[index];
 }
 
 void test(void)
@@ -25,22 +26,26 @@ void test(void)
     while (scanf("%d", &x[i]) != EOF)
         i++;
 
-    sum_arrays(x, i);
+    printf("%d",sum_arrays_total(x, i));
+
+    for (int j = 0; j < i; j++)
+        printf(" %d", sum_arrays(x, i, j));
+    printf("\n");
 }
 
 void unit_test_sum_arrays(void)
 {
-    // int a0[8] = {1, 2, 3, 4, 5, 6, 7, 8};
-    // int a1[8] = {8, 7, 6, 5, 4, 3, 2, 1};
-    // int a2[8] = {0, 0, 0, 0, 0, 1, 0, 0};
-    // int a3[8] = {1, 1, 1, 1, 1, 0, 1, 1};
-    // int a4[10] = {3, 4, 5, 3, 7, 2, 9, 3, 1, 6};
+    int a0[8] = {1, 2, 3, 4, 5, 6, 7, 8};
+    int a1[8] = {8, 7, 6, 5, 4, 3, 2, 1};
+    int a2[8] = {0, 0, 0, 0, 0, 1, 0, 0};
+    int a3[8] = {1, 1, 1, 1, 1, 0, 1, 1};
+    int a4[10] = {3, 4, 5, 3, 7, 2, 9, 3, 1, 6};
 
-    // assert(sum_arrays(a0, 8) == 7);
-    // assert(sum_arrays(a1, 8) == 7);
-    // assert(sum_arrays(a2, 8) == 0);
-    // assert(sum_arrays(a3, 8) == 1);
-    // assert(sum_arrays(a4, 10) == 7);
+    assert(sum_arrays_total(a0, 8) == 268);
+    assert(sum_arrays_total(a1, 8) == 205);
+    assert(sum_arrays_total(a2, 8) == 1);
+    assert(sum_arrays_total(a3, 8) == 13);
+    assert(sum_arrays_total(a4, 10) == 215);
 }
 
 int main(void)
